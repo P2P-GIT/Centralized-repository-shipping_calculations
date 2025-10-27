71051a2baf81ecda85c2a17b7e3a21e3374686bc
P2P-GIT <amitrsharma83@gmail.com>
2025-10-26 22:15:37 -0400

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,4 @@
-# Shipping Cost Calculator
+ Shipping Cost Calculator
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
@@ -9,4 +9,6 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
+
+# Here is a new upate by P2P-GIT
 
